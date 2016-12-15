d4e02ffc4dc12f875291a726ed68f9e7289ba3ec
denggongcai <denggongcai@xiaomi.com>
2016-12-15 14:11:04 +0800

    demo change package name

--- android/example/app/src/main/java/com/mi/setting/AppConfig.java
+++ android/example/app/src/main/java/com/mi/setting/AppConfig.java
@@ -1,29 +1,12 @@
 package com.mi.setting;
 
 public class AppConfig {
-
-    /**
-     * com.xiaomi.xhome
-     * AppID： 2882303761517461087
-     * AppKey： 5901746197087
-     * AppSecret： 9xLyXZSXy2tly64jFTqebw==
-     * http://www.xiaomi.com/xhome
-     */
-
-    /**
-     * 包名： com.mijiasdk.demo
-     * AppID： 2882303761517532465
-     * AppKey： 5161753214465
-     * AppSecret： 6ETG48uKJX33/YBcFzuHCA==
-     * http://www.xiaomi.com/mijiasdk
-     */
-
     //TODO: replace it to your own appId and appKey
-    public static final long OAUTH_APP_ID = 2882303761517461087L;
-    public static final String OAUTH_APP_KEY = "5901746197087";
-    public static final String OAUTH_REDIRECT_URI = "http://www.xiaomi.com/xhome";
+//    public static final long OAUTH_APP_ID = 2882303761517461087L;
+//    public static final String OAUTH_APP_KEY = "5901746197087";
+//    public static final String OAUTH_REDIRECT_URI = "http://www.xiaomi.com/xhome";
 
-//    public static final long OAUTH_APP_ID = 2882303761517532465L;
-//    public static final String OAUTH_APP_KEY = "5161753214465";
-//    public static final String OAUTH_REDIRECT_URI = "http://www.xiaomi.com/mijiasdk";
+    public static final long OAUTH_APP_ID = 2882303761517532465L;
+    public static final String OAUTH_APP_KEY = "5161753214465";
+    public static final String OAUTH_REDIRECT_URI = "http://www.xiaomi.com/mijiasdk";
 }
